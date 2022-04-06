6c063bd29f2ccc3d6f8fdec0391e0e0e12f57a25
Galuta Dmitrii <dvgaluta@gmail.com>
2022-04-06 23:47:39 +0300

    Commit_7

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# #
-# Действительно стек должен принимать на вход строку, а внутри преобразовываться к списку
-# Плохо называть переменную таким именем aaa = None в методе isEmpty и тем более она не используется. Вы можете просто написать len(self.list_of_stek) == 0
-# push - должен принимать на вход значение и добавлять его в стек сверху
-# pop - можно у списка просто вызвать pop(). del не нужен
-# проверка на сбалансированность работает правильно, только для внутри checing_brackets нужно использовать которые есть у стека. То есть вместо st_brackets.append(self.list_of_stek[i]) использовать push. Вместо st_brackets[-1] использовать peek
-# Никогда в python не итерируйтесь так for i in range(len(self.list_of_stek)). нужно ходить по самим объектам, а не по индексам.
-
-
-
-
-
-
-
-
-
-
-
 
 class Stack:
     def __init__(self, list_of_stek):
@@ -28,10 +10,11 @@ class Stack:
         else:
             aaa = False
         return aaa
-#push - добавляет новый элемент на вершину стека. Метод ничего не возвращает.
+
+# push - добавляет новый элемент на вершину стека. Метод ничего не возвращает.
     def push(self):
         new_element = input('ведите новый элемент')
-        self.list_of_stek.insert(0,new_element)
+        self.list_of_stek.insert(0, new_element)
 
 # pop - удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека
     def pop(self):
@@ -44,52 +27,49 @@ class Stack:
 
 
 # size - возвращает количество элементов в стеке.
-    def size (self):
+    def size(self):
         return len(self.list_of_stek)
 
     # Для стека из задания 1 проверка сбалансированности скобок.
-    def checing_brackets (self):
-        #s=list(input())
-        st_brackets=[]
+    def checing_brackets(self):
+        st_brackets = []
         for i in range(new_stack.size()):
-            if self.list_of_stek[i]=='(' or self.list_of_stek[i]=='{' or self.list_of_stek[i]=='[':
+            if self.list_of_stek[i] == '(' or self.list_of_stek[i] == '{' or self.list_of_stek[i] == '[':
                 st_brackets.append(self.list_of_stek[i])
                 continue
-            if (self.list_of_stek[i]==')' or self.list_of_stek[i]=='}' or self.list_of_stek[i]==']') and st_brackets:
-                if (st_brackets[-1]+self.list_of_stek[i]=='()') or (st_brackets[-1]+self.list_of_stek[i]=='{}') \
-                        or (st_brackets[-1]+self.list_of_stek[i]=='[]'):
+            if (self.list_of_stek[i] == ')' or self.list_of_stek[i] == '}' or self.list_of_stek[i] == ']') and st_brackets:
+                if (st_brackets[-1]+self.list_of_stek[i] == '()') or (st_brackets[-1]+self.list_of_stek[i] == '{}') \
+                        or (st_brackets[-1]+self.list_of_stek[i] == '[]'):
                     st_brackets.pop()
                 else:
                     return 'no'
-                    #exit()
+
             else:
                 return 'no'
-                #exit()
-        if st_brackets==[]:
+
+        if st_brackets == []:
             return 'Yes'
         else:
             return 'no'
 
 
 
-
-
-list_for_test = []
-
 if __name__ == '__main__':
-
-
+    start_string = input("Виудите начальную строку")
+    list_for_test = start_string.split()
     new_stack = Stack(list_for_test)
-    if new_stack.isEmpty() == True:
+    if new_stack.isEmpty():
         print("список пуст")
+        quantity = int(input("введите количествоэлеметов"))
+        for element in range(quantity):
+            new_stack.push()
     else:
         print("список имеет значения")
-    quantity = int(input("введите количествоэлеметов"))
-    for element in range(quantity):
-        new_stack.push()
-    print (list_for_test)
+
+    print(list_for_test)
+    print(new_stack.checing_brackets())
     print(new_stack.pop())
     print(new_stack.peek())
     print(new_stack.size())
-    print(new_stack.checing_brackets())
+
 
